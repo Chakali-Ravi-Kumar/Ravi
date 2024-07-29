b11d8025c315235390bf9e4ad0c86c99c14156b0
Chakali-Ravi-Kumar <156524839+Chakali-Ravi-Kumar@users.noreply.github.com>
2024-07-29 09:53:39 +0530

    Add files via upload

--- database.php
+++ database.php
@@ -1,104 +1,119 @@
 <?php 
 
 class DataBase{
-
-
-    private $db_server = "localhost";
+    private $db_host = "localhost";
     private $db_user = "root";
     private $db_pass = "";
-    private $db_name = "stu_crud";
+    private $db_name = "oops_php_crud";
+    private $db_port = 3307;
+
 
 
     private $conn = false;
     private $mysqli = "";
     private $result = array();
 
-    //to establish the connection
-    public function __construct(){
-
+    public function __construct()
+    {
         if(!$this->conn){
-            $this->mysqli = new mysqli($this->db_server,$this->db_user,$this->db_pass,$this->db_name);
+            $this->mysqli = new mysqli($this->db_host,$this->db_user,$this->db_pass,$this->db_name,$this->db_port);
             $this->conn = true;
+
             if($this->mysqli->connect_error){
-                array_push($this->result, $this->mysqli->connect_error);
+                array_push($this->result,$this->mysqli->connect_error);
+
                 return false;
             }
+           
         }else{
             return true;
         }
-
     }
 
-
-    public function insert($table, $params = array()){
-
+    public function insert($table,$params = array()){
         if($this->tableExists($table)){
-
-            echo "<pre>";
-            print_r($params);
-            echo "</pre>";
-
-            $table_colums = implode(', ',array_keys($params));
-            $table_values = implode("', '",$params);
-
-             $sql = "INSERT INTO $table ($table_colums) VALUES ('$table_values')";
-
-            if($this->mysqli->query($sql)){
+            // print_r($params);
+            $table_columns = implode(', ', array_keys($params));
+            $table_values = implode("', '", $params);
+             $sql = "INSERT INTO $table ($table_columns) VALUES ('$table_values')";
+             if($this->mysqli->query($sql)){
                 array_push($this->result, $this->mysqli->insert_id);
                 return true;
-            }else{
-
+             }else{
                 array_push($this->result, $this->mysqli->error);
                 return false;
-
-            }
-
+             }
         }else{
             return false;
         }
 
     }
 
+    
 
-    //common method to check table exist in data base or not
+    public function update($table,$params = array(),$where = null){
+        if($this->tableExists($table)){
+            // print_r($params);
+            $args = array();
+            foreach($params as $key => $value){
+                $args[] = "$key = '$value'";
+            }
+             $sql = "UPDATE $table SET ". implode(', ',$args);
+             if($where != null){
+                $sql .= " WHERE $where";
+             }
+             if($this->mysqli->query($sql)){
+                array_push($this->result, $this->mysqli->affected_rows);
+                return true;
+             }else{
+                array_push($this->result, $this->mysqli->error);
+                return false;
+             }
+        }else{
+            return false;
+        }
+    }
 
-    private function tableExists($table){
-
-        $sql = "SHOW TABLES FROM $this->db_name LIKE '$table' ";
-
-        $tableIn_db = $this->mysqli->query($sql);
-
-        if($tableIn_db){
-            if($tableIn_db->num_rows == 1) {
+    public function delete($table,$where = null){
+        if($this->tableExists($table)){
+            $sql = "DELETE FROM $table";
+            if($where != null){
+                $sql .= " WHERE $where";
+            }
+            if($this->mysqli->query($sql)){
+                array_push($this->result, $this->mysqli->affected_rows);
                 return true;
             }else{
-                array_push($result, $table."This table is not exist in this Data base");
+                array_push($this->result,$this->mysqli->error);
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+
+
+    private function tableExists($table){
+        $sql = "SHOW TABLES FROM $this->db_name LIKE '$table'";
+        $tableInDb = $this->mysqli->query($sql);
+        if($tableInDb){
+            if($tableInDb->num_rows == 1){
+                return true;
+            }else{
+                array_push($this->result, $table." Does'nt exists in DB");
                 return false;
             }
         }
-
     }
-
 
     public function getResult(){
-        $value = $this->result;
+        $val = $this->result;
         $this->result = array();
-
-        return $value;
-    }
-
-
-    //close the connection
-    public function __destruct(){
-        if($this->conn){
-            if($this->mysqli->close()){
-                $this->conn = false;
-                return true;
-            }
-        }else{
-            return false;
-        }
+        return $val;
     }
 }
+
+
+
 
 ?>
